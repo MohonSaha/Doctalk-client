5c981e1d9b9a486596d0acf5f87910b335019fed
MohonSaha <practicewithmohon@gmail.com>
2023-05-15 22:38:48 +0600

    update doctorCard component

--- src/pages/Shared/Doctors/Doctors.jsx
+++ src/pages/Shared/Doctors/Doctors.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { FaArrowRight, FaPlus } from 'react-icons/fa';
+import React, { useContext, useEffect, useState } from 'react';
+import { FaArrowRight, FaHeart, FaPlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import DoctorsCard from './DoctorsCard';
+import { authContext } from '../../../providers/AuthProviders';
 
 const Doctors = () => {
+
+    const {user} = useContext(authContext);
+
 
     const [currentPage, setCurentPage] = useState(0);
     const [totalDoctors, setTotalDoctors] = useState(null)
@@ -28,13 +32,24 @@ const Doctors = () => {
 
 
     const [doctors, setDoctors] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:5000/allDoctors')
-            .then(res => res.json())
-            .then(data => {
-                setDoctors(data)
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/allDoctors')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setDoctors(data)
+    //         })
+    // }, [])
+
+    useEffect( ()=>{
+        async function fetchData() {
+            const response = await fetch (`http://localhost:5000/allDoctors?page=${currentPage}&limit=${doctorPerPage}`)
+            const data = await response.json()
+            setDoctors(data)
+        }
+        fetchData()
+    } , [currentPage, doctorPerPage])
+
+
 
     const options = [2, 4, 6, 8];
     function handleSelectChange(event) {
@@ -47,22 +62,22 @@ const Doctors = () => {
         <>
             <div className='mb-60'>
                 <div className='text-center mb-16 -mt-12'>
-                    <h3 className='font-bold text-xl text-info'>What We do</h3>
-                    <h1 className='font-bold md:text-6xl text-5xl text-[#017f7f]'>Our Services</h1>
+                    <h3 className='font-bold text-xl text-info'>Our Hero</h3>
+                    <h1 className='font-bold md:text-6xl text-5xl text-[#017f7f]'>Specialist Doctor</h1>
                 </div>
 
                 <div className='flex justify-between mx-6 md:mx-28 mb-6'>
 
-                    <p className=' text-[#017f7f] font-semibold  cursor-pointer'><Link className='flex items-center' to='/addDiseases'>Add Services <FaPlus className='ml-3'></FaPlus></Link></p>
+                    <p className=' text-[#017f7f] font-semibold  cursor-pointer'><Link className='flex items-center' to='/addDiseases'>Add Doctors <FaPlus className='ml-3'></FaPlus></Link></p>
 
-                    <p className=' text-[#017f7f] font-semibold  flex items-center cursor-pointer'>Swipe Right <FaArrowRight className='ml-3'></FaArrowRight></p>
+                   { user ?  <p className=' text-[#017f7f] font-semibold  cursor-pointer'><Link className='flex items-center' to='/addDiseases'>Fevourite List<FaHeart className='ml-3'></FaHeart></Link></p> : ''}
 
 
                 </div>
 
                 <div className="grid md:grid-cols-2 gap-8 grid-cols-1 mx-6 md:mx-28">
                     {
-                        doctors.map(doctor => <DoctorsCard
+                        doctors?.map(doctor => <DoctorsCard
                             key={doctor._id}
                             doctor={doctor}
                         ></DoctorsCard>)
@@ -75,10 +90,10 @@ const Doctors = () => {
                 <div className="pagination text-end mx-6 md:mx-28 mt-6">
                     <p><small>Current Page: {currentPage}</small></p>
                     {
-                        pageNumbers.map(number => <button className={`btn bg-white ml-3 text-black ${currentPage === number ? "bg-red-500": ''}`} 
+                        pageNumbers.map(number => <button className={`btn bg-white ml-3 border-[#017f7f] ${currentPage === number ? "bg-teal-700 text-white": 'text-black'}`} 
                         key={number}
                         onClick={() => setCurentPage(number)}
-                        >{number}</button>)
+                        >{number + 1}</button>)
                     }
 
                     <select className='ml-4' value={doctorPerPage} onChange={handleSelectChange}>
--- src/pages/Shared/Doctors/DoctorsCard.jsx
+++ src/pages/Shared/Doctors/DoctorsCard.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
+import { FaArrowRight, FaEllipsisH } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const DoctorsCard = ({ doctor }) => {
 
-    console.log(doctor);
-    const {name, _id, photo, degree} = doctor;
+    const { name, _id, photo, degree, speciality } = doctor;
 
+
+    const handleFavourite = (id) =>{
+        console.log(id);
+    }
 
     return (
         <div className=''>
-            <div className="card card-side bg-base-100 shadow-xl">
-                <figure><img src={photo} alt="Movie" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
+            <div className="card border-4 border-base-300 card-side bg-base-200 h-[270px]">
+                <figure className='w-1/3'>
+                    <img className='h-full w-48' src={photo} alt="Movie" />
+                </figure>
+                <div className="card-body w-2/3">
+                    <h2 className="card-title text-xl">{name}</h2>
+                    <p className='font-medium'><small>{degree}</small></p>
+                    <p className='font-medium text-red-600'>{speciality}</p>
+                    <div className="w-full">
+
+
+                    <div className="card-actions flex justify-between items-center">
+                        <span className='font-bold cursor-pointer  text-[#09c3d0]'>
+                            <Link className='flex items-center' to={`/servicesDetails/${_id}`}>
+                                See Details
+                                <FaArrowRight className='ml-2 -mb-1'></FaArrowRight>
+                            </Link>
+                        </span>
+
+
+
+                        <div className="dropdown dropdown-top dropdown-end">
+                            <label tabIndex={0} className=" m-1"><FaEllipsisH className='text-[#09c3d0]'></FaEllipsisH></label>
+                            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-[#017f7f] rounded-box w-52">
+
+                                <li className='text-white'>
+                                    <span className='font-semibold cursor-pointer'><Link className='w-full' to={`/services/${_id}`}>Appointment</Link></span>
+                                </li>
+
+                                <li className='text-white'>
+                                    <span onClick={() => handleFavourite(_id)} className='font-semibold cursor-pointer '>Add Favourite</span>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                    </div>
                     </div>
                 </div>
             </div>
