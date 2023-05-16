693d7e6cd50444d81a7d8fd2f04d45345922aa0d
MohonSaha <practicewithmohon@gmail.com>
2023-05-16 08:15:58 +0600

    implement doctor details

--- src/pages/Shared/Doctors/DoctorsDetails.jsx
+++ src/pages/Shared/Doctors/DoctorsDetails.jsx
@@ -6,22 +6,22 @@ const DoctorsDetails = () => {
 
     const { id } = useParams()
     console.log(id);
-    const [details, setDetails] = useState([]);
+    const [doctorDetails, setDoctorDetails] = useState([]);
     useEffect(() => {
         fetch(`http://localhost:5000/doctorsDetails/${id}`)
             .then(res => res.json())
             .then(data => {
-                setDetails(data);
+                setDoctorDetails(data);
             })
 
     }, [])
 
-    const { name, photo, _id, work_place, First_fee, fee, degree, rating, speciality, working_hour } = details;
+    const { name, photo, _id, work_place, First_fee, fee, degree, rating, speciality, working_hour, details, Chamber } = doctorDetails;
 
     return (
-        <div className='grid md:grid-cols-2 grid-cols-1 md:mx-28 min-h-screen'>
-            <div>
-                <div className="card w-96 bg-base-100 shadow-xl">
+        <div className='grid md:grid-cols-2 grid-cols-1 md:mx-28 min-h-screen gap-8 md:gap-0'>
+            <div className='w-2/3 m-auto md:m-0'>
+                <div className="card  bg-base-100 shadow-xl">
                     <figure>
                         <img className='w-full h-80' src={photo} alt="Shoes" />
                     </figure>
@@ -36,13 +36,40 @@ const DoctorsDetails = () => {
 
 
 
-            <div>
+            <div className=' h-[calc(85vh-20px)] overflow-y-scroll px-8'>
 
                 <div>
-                    
+                    <div className="card w-full bg-base-300 shadow-xl">
+                        <figure className="px-4 pt-10">
+                            <h2 className='text-2xl font-semibold'>About {name}</h2>
+                        </figure>
+                        <hr className='border-1 border-stone-400 mt-2' />
+                        <div className="card-body items-center text-center -mt-4">
+                            <p>{details}</p>
+                        </div>
+                    </div>
                 </div>
 
-                
+                <div>
+                    {
+                        Chamber?.map(chem => <div className="card bg-base-300 w-full my-8">
+                            <figure className="px-4 pt-10">
+                                <h2 className='text-lg text-[#017f7f] font-bold'>{chem.place}</h2>
+                            </figure>
+                            <hr className='border-1 border-stone-400 mt-2' />
+                            <div className="card-body items-center text-center -mt-4">
+                                <p className='font-medium'>Address: {chem.address}</p>
+                                <p className='font-medium'>Address: {chem.time}</p>
+                                <p className='font-medium'>Address: {chem.phone}</p>
+                                <div className="card-actions">
+                                    <button className="btn bg-[#017f7f] border-[#017f7f] text-white">Appointment</button>
+                                </div>
+                            </div>
+
+                        </div>)
+                    }
+                </div>
+
             </div>
         </div>
     );
