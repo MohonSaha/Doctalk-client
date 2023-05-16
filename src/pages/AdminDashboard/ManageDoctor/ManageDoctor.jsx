a9c5cf6474bedf19f9a1d2cc4df38ccba19f8e66
MohonSaha <practicewithmohon@gmail.com>
2023-05-16 14:20:43 +0600

    implement delete operaton with doctor

--- src/pages/AdminDashboard/ManageDoctor/ManageDoctor.jsx
+++ src/pages/AdminDashboard/ManageDoctor/ManageDoctor.jsx
@@ -1,32 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { stateContext } from '../../../providers/StateChange';
 
 const ManageDoctor = () => {
 
     const [doctorsData, setDosctorsData] = useState([])
+
+    const {control, setControl} = useContext(stateContext);
+
+    const handleDelete = (id) =>{
+        fetch(`http://localhost:5000/removeDoctor/${id}`,{
+            method: "DELETE",
+            headers: {
+                "content-type" : "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data?.deletedCount > 0){
+                setControl(!control)
+            };
+        })
+        console.log(id);
+    }
+
+
+
     useEffect(() => {
         fetch('http://localhost:5000/allDoctorsData')
             .then(res => res.json())
             .then(data => {
                 setDosctorsData(data)
             })
-    }, [])
+    }, [control])
 
     
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mx-6 md:mx-16'>
             {
-                doctorsData.map(doctor => <div key={doctor._id}>
-                    <div className="card w-96 bg-base-200 m-auto">
-                        <figure><img className='w-full h-72' src={doctor.photo} alt="Shoes" /></figure>
+                doctorsData.map((doctor, i) => <div key={doctor._id}>
+                    <div className="card w-96 bg-base-200 m-auto  h-[560px]">
+                        <figure>
+                            <img className='w-full h-72' src={doctor.photo} alt="Shoes" /></figure>
                         <div className="card-body">
+                           
                             <h2 className="card-title">{doctor.name}</h2>
                             <p>{doctor.degree}</p>
                             <p className='text-red-600'>{doctor.speciality}</p>
                             <div className="card-actions justify-end">
                                 <button className="btn bg-[#017f7f] border-[#017f7f] text-white">Update</button>
-                                <button className="btn bg-red-500 border-red-500 text-white">Delete</button>
+                                <button onClick={() => handleDelete(doctor._id)} className="btn bg-red-500 border-red-500 text-white">Delete</button>
                             </div>
+                            <p>Serial No: {i + 1}</p>
                         </div>
                     </div>
                 </div>)
